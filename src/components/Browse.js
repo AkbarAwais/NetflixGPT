22a9ef00bb218dd309de079a16c511822306483d
Akbar Awais <88664220+AkbarAwais@users.noreply.github.com>
2024-11-27 01:47:48 +0530

    More error handling

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header';
+import { onAuthStateChanged } from 'firebase/auth';
+import { addUser, removeUser } from '../utils/userSlice';
+import { useDispatch } from 'react-redux';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Browse = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isLoggedUser, setIsLoggedUser] = useState(true)
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                dispatch(addUser({ uid: user.uid, email: user.email, displayName: user.displayName }))
+                setIsLoggedUser(true);
+            } else {
+                dispatch(removeUser(user))
+                setIsLoggedUser(false);
+                navigate('/')
+            }
+        })
+    }, [])
+
     return (
         <div>
-            <Header />
+            <Header data={isLoggedUser} />
         </div>
     )
 }
