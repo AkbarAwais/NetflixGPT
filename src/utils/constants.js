11eafa4e1775586dc35ad2d73fb54bd26ea3b5ec
Akbar Awais <88664220+AkbarAwais@users.noreply.github.com>
2024-12-01 20:48:00 +0530

    img

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const NETFLIX_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/4690cab8-243a-4552-baef-1fb415632f74/web/IN-en-20241118-TRIFECTA-perspective_0b813abc-8365-4a43-a9d8-14c06e84c9f3_small.jpg";
+export const NETFLIX_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/2bcf01ee-7ef6-4930-b0d5-c6863853c461/web/IN-en-20241125-TRIFECTA-perspective_a47db038-756f-4f26-b1f7-cfc882b98746_large.jpg";
 export const GET_MOVIE_LIST = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
 export const MAIN_VIDEO_URL = (movieId) => `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`;
 export const MAIN_TRAILER_URL = (trailerId) => `https://www.youtube.com/embed/${trailerId}?&autoplay=1&mute=1&modestbranding=1&controls=0&loop=1&rel=0&playlist=${trailerId}`;
